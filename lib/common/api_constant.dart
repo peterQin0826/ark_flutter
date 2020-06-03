d25c525e47c4324b1980f081ab28ee5010f4af1b
xiaopengqin <peterxpq@163.com>
2020-06-03 17:39:45 +0800

    提交

--- lib/common/api_constant.dart
+++ lib/common/api_constant.dart
@@ -1,12 +1,12 @@
 class ApiConstant {
   ///生产环境
-//  static const String HOST="http://ark.ubi-creation.com/";
+  static const String HOST="http://ark.ubi-creation.com/";
 
   ///预生产环境
 //  static const String HOST="http://test.ubi-creation.com/";
 
   ///本地环境
-  static const String HOST = "http://192.168.1.119:18889/";
+//  static const String HOST = "http://192.168.1.119:18889/";
 
 
 }
